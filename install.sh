75017ba125c10b519dba8ea87962ebca52025791
lasaxori <57449725+lasaxori@users.noreply.github.com>
2020-05-17 13:39:04 +0300

    Delete install.sh

--- install.sh
+++
@@ -1,3 +0,0 @@
-cd $HOME
-pkg install python2 -y&&pkg install git -y&&pip2 install requests
-git clone https://github.com/lasaxori/gazi.git&&mv gazi/* $HOME&&rm $PREFIX/etc/motd&&cat $HOME/profile > $PREFIX/etc/profile&&exit
