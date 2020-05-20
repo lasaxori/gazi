bcc8ae2d44eba31e952f5cee119d106621e3ace0
lasaxori <57449725+lasaxori@users.noreply.github.com>
2020-05-20 22:42:35 +0300

    Add files via upload

---
+++ gazi.py
@@ -0,0 +1,195 @@
+#! /usr/bin/env python2
+# -*- coding: utf-8 -*-
+import os, time
+os.system("clear")
+W = "\033[0m"
+G = '\033[32;1m'
+R = '\033[31;1m'
+k="\033[91m"
+kmavi="\033[94m"
+y="\033[92m"
+kizil="\033[101m"
+norm="\033[1;37;40m"+"\033[92m"
+sari="\033[43m"
+s="\033[33m"
+menu=["\033[1;37;40m"+"\033[92m"+'   '  ,         
+'           ############          ##       ############      '+kizil+R+'...'+norm,
+'         ## '+G+'                   #   #                 #      '+R+kizil+'...'+norm,
+'        # '+G+'                    #     #               #       '+s+sari+'...'+norm,
+'       # '+G+'                    #       #             #        '+sari+s+'...'+norm,
+'       #     ########## '+G+'    #         #          #          '+sari+s+"""..."""+norm,
+'       #  	       #  '+G+'  ############      #####         '+sari+s+"""..."""+norm,
+'        #	       # '+G+'  #           #      #	            '+sari+s+"""..."""+norm,
+'         #	      #   #             #    # 	            '+sari+s+"""..."""+norm,
+'           #         #   #              #   #		    '+sari+s+"""..."""+norm,
+'    	    #########   #               #  ############     '+sari+s+"""..."""+norm,
+' ',    	     
+kmavi+'     _____________________________________________________________',
+r'   //                                                             \\',
+'  ||'+G+' GAZI HAZIRLIK DONEM SONU NOT HESAPLAMA PROGRAMINA HOSGELDINIZ '+kmavi+'||',
+r'   \\_____________________________________________________________//',
+'                    ||                           ||          ',    
+'                    ||'+G+'       VERSION= 0.32 '+kmavi+'      ||',
+kmavi+'             ||========================================||  ',
+'             ||                                        ||',
+'             ||'+G+'    OTOMATİK HIZLI HESAPLAMA ICIN   "'+s+'1'+G+'" '+kmavi+'||',
+'             ||'+G+'     MANUEL YAVAS HESAPLAMA ICIN    "'+s+'2'+G+'" '+kmavi+'||',
+'             ||'+G+'  GAZI HOCALARI HAKKINDA BILGI ICIN "'+s+'3'+G+'" '+kmavi+'||',
+'             ||'+G+'            CIKMAK ICIN ISE         "'+s+'q'+G+'" '+kmavi+'||',
+'             ||========================================||'+'\033[91m']
+
+menu2=["""\033[91m""" +"""                            ............""",
+"""                           || """+"""\033[101m""" +"""\033[93m"""+"""!DIKKAT!"""+    """\033[1;37;40m"""+     """\033[91m""" +""" ||""",
+"""  #••••••••••••••••••••••••••          ••••••••••••••••••••••••••••#""",
+"""  #   """+"""\033[33m"""+"""EGER NOTUNUZ GIRILMEMIS ISE BOS BIRAKINIZ, 0 ise 0 yaziniz!"""+"""\033[91m"""+"""  #""",
+"""  #########------------------------------------------------#########"""+"\033[92m"," "]
+sablon=["ADI","SOYADI","ANNE ADI","BABA ADI","DOGUM TARIHI","ADRESI","DOGUM YERI","TC KIMLIK NO","Tel No"]
+
+sablon_k=["ADI","SOYADI","KIZLIK SOYADI","ANNE ADI","BABA ADI","DOGUM TARIHI","ADRESI","DOGUM YERI","TC KIMLIK NO","Tel No"]
+
+vizeuyari="\033[93m"+"\n SIMDI VIZE NOTLARINIZI YAZINIZ\n"+"\nIPUCU= VIZE NOTUNUZU  "+"\033[101m"+"14+13+19+10.5"+"\033[0m"+"\033[1;37;40m"+"\033[33m"+"  seklinde de yazbilirsiniz\n"+"\033[92m"
+tasak="Tassak mi geciosun aq ya bu nedir ya:{}\n"
+vizetoplam="0123456789.q+"
+
+table=kmavi+"""
+      
+  ||==================================||
+  ||"""+G+"""  for turn menu  >>   "q" or "Q"  """+kmavi+"""||
+  ||"""+G+"""  for return list >>  "l" or "L"  """+kmavi+"""||
+  ||==================================||
+    """+G
+
+eski_tablo=['  ',kmavi+'             ||========================================||  ',
+'             ||                                        ||',
+'             ||'+G+' YALNIZCA BIR DONEM ORTALAMASI ICIN "'+s+'1'+G+'" '+kmavi+'||',
+'             ||'+G+'   HER IKI DONEM ORTALAMASI ICIN    "'+s+'2'+G+'" '+kmavi+'||',
+'             ||'+G+'   HER IKI DONEM  HIZLI HESAP ICIN  "'+s+'3'+G+'" '+kmavi+'||',
+'             ||'+G+'             ANA MENU ICIN          "'+s+'g'+G+'" '+kmavi+'||',
+'             ||========================================||'+'\033[91m']
+        
+
+
+a=0
+v_3=00
+q_14=545
+ort_2=55555
+ort_net=5558
+
+
+while True:
+    a=0
+   # os.system("clear")
+    time.sleep(0.031)
+    print(" ")
+    time.sleep(0.031)
+    for ana in menu:
+        print(ana)
+        time.sleep(0.031)
+    print("   ")
+    time.sleep(0.031)
+
+    print(G)
+    time.sleep(0.037)
+    oc=1
+    oc2=1
+    islem=raw_input("islem no: ").replace(" ","")
+
+    if islem=="3":
+        if islem=="3":
+            h=open("hocalar.txt","r")
+            b=open("hoca_b.txt","r")
+            print("")
+            for sira, i in enumerate(h,1):
+                time.sleep(0.05)
+                print("\033[101m"+"("+"\033[1;37;40m"+"\033[33m"+str(sira).zfill(2)+"\033[101m" +")"+"\033[92m"+"\033[1;37;40m"+" "+"\033[92m"+i.replace("\n","") )
+            h.seek(0)
+            b.seek(0)
+            print('')
+            time.sleep(0.1)
+            print('')
+            time.sleep(0.1)
+            print("        HAKKINDA BILGI SAHIBI OLMAK ISTEDIGINIZ")
+            time.sleep(0.1)
+            print("          HOCAYI SECINIZ")
+            for desk in table.split("\n"):
+                time.sleep(0.1)
+                print(desk)
+            time.sleep(0.1)
+            while True:
+                
+                ist=raw_input("\n\n NUMARAYI GIRINIZ: ")
+                print("\n")
+                if ist=="l" or ist=="L":
+                    for sira, i in enumerate(h,1):
+                        time.sleep(0.05)
+                        print("\033[101m"+"("+"\033[1;37;40m"+"\033[33m"+str(sira).zfill(2)+"\033[101m" +")"+"\033[92m"+"\033[1;37;40m"+" "+"\033[92m"+i.replace("\n","") )
+                    h.seek(0)
+                    b.seek(0)
+                    
+                if ist=="q" or ist=="Q":
+                    oc=0
+                    break
+                try:
+                    int(ist)
+                except:
+                    time.sleep(0.1)
+                    print("\n listedeki sira numarasini gir")
+                    
+                    continue
+                if not 0<int(ist)<68:
+                    print("\n liste sira numarasini gir")
+                    continue
+                for sira , i in enumerate(b,1):
+                    if sira==int(ist):
+                        i=i.split("|")
+                        if i[0]=="E":
+                            for sayi in range(9):
+                                time.sleep(0.04)
+                                print  W + '[' + G + sablon[sayi] + W + ']' + R + ' >> ' + W + i[sayi+1] 
+                
+                    
+                        if i[0]=="K":
+                            for sayi in range(10):
+                                time.sleep(0.04)
+                                print  W + '[' + G + sablon_k[sayi] + W + ']' + R + ' >> ' + W + i[sayi+1]
+                time.sleep(0.02)
+                print("\033[1;37;40m")
+                time.sleep(0.02)
+                print("\033[92m")
+                time.sleep(0.02)
+                h.seek(0)
+                time.sleep(0.02)
+                b.seek(0)
+                time.sleep(0.02)
+                continue
+            if oc==0:
+                print("\nmenuye donuluyor..\n")
+                oc=1
+                continue
+    if islem=="q" or "Q"==islem:
+        quit()
+    
+    if islem=="1":
+        os.system("python2 ygazi.py")
+        continue
+    if islem=="2":
+        for ana in eski_tablo:
+            print(ana)
+            time.sleep(0.031)
+        for ana in menu2:
+            print(ana)
+            time.sleep(0.031)
+        while True:
+            islem=raw_input("islem no: ").replace(" ","")
+            if islem=="1":
+                os.system("python2 1.py")
+                break
+            if islem=="2":
+                os.system("python2 2.py")
+                break
+            if islem=="3":
+                os.system("python2 3.py")
+                break
+            if islem=="g" or islem=="G" or islem=='q' or islem=="Q":
+                break
+                
---
+++ ygazi.py
@@ -0,0 +1,250 @@
+# -*- coding: utf-8 -*-
+import requests, time 
+from total import *
+G = '\033[32;1m'
+R = '\033[31;1m'
+kmavi="\033[94m"
+S="\033[33m"
+norm="\033[1;37;40m"+"\033[92m"
+url='http://ydyosistem.gazi.edu.tr/LoginScript.php'
+url_vize='http://ydyosistem.gazi.edu.tr/admin.php?Process=VizeBilgi'
+url_quiz='http://ydyosistem.gazi.edu.tr/admin.php?Process=QuizBilgi'
+
+
+
+while True:
+    no=raw_input("""  
+        OGRENCI NUMARANIZ: """).strip()
+    if no=="g" or no=="G" or no=="q" or no=="Q":
+        exit()
+    try:
+        no=int(no)
+        no=str(no)
+        assert len(no)==9, "fhata"
+    except:
+        print(R+"\n  [!] hatali giris\n"+G)
+        continue
+    break
+data={"username":no,"password":no[-6:],'ogr':'1'}
+with requests.Session() as ses:
+    print(S+"\n siteye giris yapiliyor..")
+    try:
+       r = ses.post(url, allow_redirects=True, data=data, timeout=10)
+    except:
+        print(R+"\n [!] bu islem icin internet baglantisi gerekir\n"+G)
+        time.sleep(6)
+        exit()
+    
+    if not """<meta http-equiv="Refresh" content="0; URL=admin.php">""" in r.text:
+        print(R+"\n  [!] boyle bir ogrenci bulunamadi\n")
+        time.sleep(5)
+        exit()
+    print(G+" siteye giris basarili  %15")
+    print(S+'\n quiz notlari aliniyor..')
+    rrr=ses.get(url_quiz, allow_redirects=True , headers=r.headers, timeout=15)
+    
+    print(G+' quiz notlari alindi     %60')
+    icerik_quiz=rrr.text.encode('utf-8')
+    print(S+'\n vize notlari aliniyor..')
+    rr=ses.get(url_vize, allow_redirects=True , headers=r.headers, timeout=15)
+    print(G+" vize notlari alindi     %100")
+    
+    icerik_vize=rr.text.encode('utf-8')
+
+    
+
+
+quiz_liste=saf_quiz(icerik_quiz)
+
+#vize ixerik hesaplama
+
+vize_liste=saf_vize(icerik_vize)
+
+  
+###  ilkd notunu hesplayak  ######
+ilkdv=[]
+
+ilkdv.append(vize_liste[0])
+ilkdv.append(vize_liste[1])
+ilkdv.append(vize_liste[2])
+
+ilkdq=[]
+for i in range(14):
+    ilkdq.append(quiz_liste[i])
+
+ilkdq=topla(ilkdq)
+
+ilkdv=topla(ilkdv)
+ilkd=ilkdq/14*3.0/10 + float(ilkdv/3)*7/10
+######################
+
+# vize
+
+del vize_liste[0]
+del vize_liste[0]
+del vize_liste[0]
+kalan_v=kalan_vize(vize_liste)
+
+vize_t=topla(vize_liste)
+
+
+# quiz
+
+for i in range(14):
+    del quiz_liste[0]
+
+    
+quiz_t=topla(quiz_liste)
+
+    
+kalan_q=kalan_quiz(quiz_liste)
+
+
+
+if kalan_v==3 and kalan_q==14:
+    ort_2=ilkd
+elif kalan_v==3:
+    
+    ort_2=float(quiz_t)/(14-kalan_q)
+elif kalan_q==14:
+    ort_2=float(vize_t)/(3-kalan_v)
+else: 
+    ort_2=float(vize_t)/float(3-kalan_v)*7.0/10 + float(quiz_t)/float(14-kalan_q)*3.0/10
+
+ort_net=ilkd*4.0/10 + ort_2*6.0/10
+
+
+
+if kalan_q==0 and kalan_v==0:
+    if  ort_2>=75:
+        print(kmavi+"""
+
+                 ##############################################
+   		#				               #
+		#"""+G+'''       HELAL OLSUN LAN FINALSIZ GECTIN!'''+kmavi+'''       #
+                # '''+G+'''         QOPEK GIBI CALISTIN DIMI LAN  '''+kmavi+'''      #
+		#     '''+G+'''           SENI HAYVAN HERIF   '''+kmavi+'''          #
+		#                       		       #
+		#   '''+G+'''            CONGRATULATIONS	'''+kmavi+'''	       #
+		#	'''+G+'''                  	NOTUN: {}  '''.format(str(ort_net)[:4])+kmavi+'''  #
+		#					       #
+		 ##############################################
+
+'''+G)
+        raw_input("[ent]")
+        exit()
+    elif ort_net>=45 :
+        print(kmavi+"""
+
+                 ##############################################
+   		#				               #
+		#"""+G+"""    KANKA IPIN UCUNDASIN HEMEN AC KITABINI   """+kmavi+""" #
+                #"""+G+"""      BASLA DERS CALISMAYA KAYBEDECEK """+kmavi+"""        #
+		#   """+G+"""        1 SANIYEN YOK ARTIK.  """+kmavi+"""             #
+		#                       		       #
+		# """+G+"""               NOTUN: {}	 """.format(str(ort_net)[:4])+kmavi+"""            #
+		#_____________________________    	       #
+		√"""+G+"""finalden en az {} almalisin""".format(str(float(300-2*ort_net)/3)[:4])+kmavi+"""|                #
+		 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#################
+
+"""+G)
+        raw_input("[ent]")
+        exit()
+    else:
+        print(kmavi+"""
+
+                 ###############################################
+   		#						#
+		#  """+G+"""  aptal herif bosa yasiyosun keske olsen   """+kmavi+"""  #
+                #  """+G+"""    finale kaldin derdim ama sen direk   """+kmavi+"""    #
+		#  """+G+"""        hazirlikta kaldin agla   """+kmavi+"""            #
+		#                       			#
+		#  """+G+"""              NOTUN: {}	   """.format(str(ort_net)[:4])+kmavi+"""           #
+		#____________________________  			#
+		√ """+G+"""finalden en az {} almalisin""".format(str(float(300-2*ort_net)/3)[:2])+kmavi+"""|			#
+                √ """+G+"""tabi boyle bisi mumkunse   """+kmavi+"""|			#
+		 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~|##################
+
+"""+G)
+        raw_input("[ent]")
+        exit()
+    
+    
+#Sinavlar Bitmemis İse
+if ort_2>71:
+    print(kmavi+"""
+
+                 ###############################################
+   		#						#
+		#   """+G+"""   2.DONEME SUPER BASLAMİSSİN AFERİM      """+kmavi+"""  #
+                # """+G+""" EGER BU DONEM ORTALAMANİ 75 USTUNDE TUTMAYİ  #
+		#   """+G+"""        BASARİRSAN FİNALSİZ GECERSİN  """+kmavi+"""      #
+		#                       			#
+		#      """+G+"""          NOTUN: {}	 """.format(str(ort_net)[:4])+kmavi+"""             #
+		#    			  			#
+		|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+                |"""+G+""" BU DONEMKİ SİNAVLARDAN MİNİMUM {} alsan yeter""".format(str((750 - float(vize_t*7)/3 - float(quiz_t*3)/14) / (float(kalan_v*7)/3 + float(kalan_q)/14*3))[:4])+kmavi+"""
+		|~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~|
+
+"""+G)
+    raw_input("[ent]")
+    exit()
+    
+
+else:
+    while True:
+        try:
+            hdf=raw_input("""
+            yil sonu genel basari
+            puani hedefin kac   = """).replace(",",".").replace(" ","")
+            if hdf=="":
+                print("""
+                
+              HEDEFI OLMAYAN BIRINE 
+	        HICBIR PROGRAM YARDIM EDEMEZ.
+	        """)
+	        time.sleep(5)
+                continue
+            hdf=float(hdf)
+            if not 100>=hdf>=0:
+                continue
+        except:
+            if hdf=="q" or hdf=="Q":
+                quit()
+            del hdf
+            continue
+        break
+    if not 100>=float((float(hdf*10 - ilkd*4)*10/6 - quiz_t*3/14 - float(vize_t*7)/3) / (float(kalan_v*7)/3 + float(kalan_q*3)/14))>=0:
+        print(kmavi+"""
+
+                 ###########################################
+   		#				            #
+		#   """+G+"""  UZGUNUM ÇARLİ AMA BU NOTLARLA   """+kmavi+"""      #
+                #    """+G+"""   HEDEFİNE ULASMAN İMKANSİZ   """+kmavi+"""        #
+                #	      ________________  	    #
+         	#             \\  """+G+"""NOTUN: {} """.format(str(ort_net)[:4])+kmavi+"""/	          #
+    		###############\\____________/###############
+               		        
+				
+"""+G)
+        raw_input("[ent]")
+        exit()
+    else:
+        print(kmavi+"""
+ 
+                 ##############################################
+   		#				               #
+		#"""+G+"""    FAZLA KASMANA GEREK YOK ONUMUZDEKİ """+kmavi+"""       #
+                #  """+G+"""    SİNAVLARDAN MİNİMUM {} ALSAN HEDEFİNE """.format(str((float(hdf*10 - ilkd*4)*10/6 - quiz_t*3/14 - float(vize_t*7)/3) / (float(kalan_v*7)/3 + float(kalan_q*3)/14))[:4])+kmavi+"""#
+		#    """+G+"""         RAHAT ULASABİLİRSİN    """+kmavi+"""          #
+		#                       		       #
+		#   """+G+"""             NOTUN: {}	  """.format(float(str(ort_net)[:4]))+kmavi+"""             #
+		#_____________________________________________ #
+		 ##############################################
+		 
+
+		 
+""")
+        raw_input(G+"[ent]")
+        exit()
+exit()
